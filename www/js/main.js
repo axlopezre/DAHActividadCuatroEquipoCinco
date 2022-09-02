aa9321467f825518294b985b75c52790d541ef3b
Sketchead <megamam_atxel@hotmail.com>
2022-09-02 16:07:17 -0600

    Documentación y correción de errores

--- www/js/main.js
+++ www/js/main.js
@@ -8,17 +8,14 @@ $(function () {
         $('.preloader').delay(500).fadeOut(500);
     });
 
-
     //===== Sticky
 
     $(window).on('scroll', function (event) {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
         }
     });
 
@@ -33,7 +30,7 @@ $(function () {
 
         scrollLink.each(function () {
 
-            var sectionOffset = $(this.hash).offset().top - 73;
+            var sectionOffset = $(this.hash).offsetTop - 73;
 
             if (sectionOffset <= scrollbarLocation) {
                 $(this).parent().addClass('active');
